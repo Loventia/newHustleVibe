4e2bc9b7e220613699b50413b1d97384b5080f2a
Loventia <48926260+Loventia@users.noreply.github.com>
2022-07-18 13:27:39 +0200

    Added AddCategoryData

--- HustleVibeSln/HustleVibe/HustleVibe/Helpers/AddCategoryData.cs
+++ HustleVibeSln/HustleVibe/HustleVibe/Helpers/AddCategoryData.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using Firebase.Database;
+using Firebase.Database.Query;
+using System.Linq;
 using HustleVibe.Model;
 
      
@@ -16,8 +17,67 @@ namespace HustleVibe.Helpers
         public AddCategoryData() 
         {
             client = new FirebaseClient("https://hustlevibe-193fb-default-rtdb.firebaseio.com/");
-            Categories= new List<Category>
+            Categories = new List<Category>()
+            {
+
+                new Category()
+                {
+                    CategoryID=1,
+                     CategoryName="Agriculture",
+                     CategoryPoster="Mainburger",
+                     ImageUrl= ""
+
+
+                },
+
+                new Category()
+                {
+                    CategoryID=2,
+                     CategoryName="Home services",
+                     CategoryPoster="Mainburger",
+                     ImageUrl= ""
+
+                },
+                new Category()
+                {
+                    CategoryID=3,
+                     CategoryName="Auto services",
+                     CategoryPoster="Mainburger",
+                     ImageUrl= ""
+                },
+                new Category()
+                {
+                    CategoryID=4,
+                     CategoryName="Food",
+                     CategoryPoster="Mainburger",
+                     ImageUrl= ""
+                },
+                new Category()
+                {
+                    CategoryID=5,
+                     CategoryName="Beauty",
+                     CategoryPoster="Mainburger",
+                     ImageUrl= ""
+                }
+
+            };
         }
-       
+
+        private async Task AddCategoriesAsync()
+        {
+            var categories = (await client.Child("Categories"))
+                .OnceAsync<Category>()
+                .Select(c => new Category
+
+                {
+                    CategoryID = c.Object.CategoryID,
+                    CategoryName = c.Object.CategoryName,
+                    CategoryPoster = c.Object.CategoryPoster,
+                    ImageUrl = c.Object.ImageUrl
+                }).ToList();
+            return Categories;
+        }
+
     }
 }
+
