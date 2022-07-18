0ca19c65ec7f6173dcbc54624da48203965aa7e1
ZeeLugalo <4271363@myuwc.ac.za>
2022-07-18 15:02:12 +0200

    SettingsPageXaml

--- HustleVibeSln/HustleVibe/HustleVibe/Views/SettingsPage.xaml.cs
+++ HustleVibeSln/HustleVibe/HustleVibe/Views/SettingsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using HustleVibe.Helpers;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,26 @@ using Xamarin.Forms.Xaml;
 
 namespace HustleVibe.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+
     public partial class SettingsPage : ContentPage
     {
         public SettingsPage()
         {
             InitializeComponent();
+
         }
+       async void ButtonCategories_Clicked(Systems.Object sender, System.EventArgs e)
+        {
+            var acd = new AddCategoryData();
+            await acd.AddCategoryAsync();
+        }
+        
+       async void ButtonProducts_Clicked(Systems.Object sender, System.EventArgs e)
+        {
+            var afd = new AddItemData();
+            await acd.AddItemAsync();
+        }
+        
     }
-}
+      
+    }
