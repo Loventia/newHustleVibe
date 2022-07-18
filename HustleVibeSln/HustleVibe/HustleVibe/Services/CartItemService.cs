7a459cc40ef3a06609ee00c570c725430521b9b2
peternontembeko <peternontembeko22@gmail.com>
2022-07-18 12:39:02 +0200

    CartItemService

---
+++ HustleVibeSln/HustleVibe/HustleVibe/Services/CartItemService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HustleVibe.Services
+{
+    public  class CartItemService
+    {
+        public int GetUserCartCount()
+        {
+
+            return 0;
+        }
+
+        public void RemoveItemsFromCart()
+        {
+
+        }
+
+    }
+}
